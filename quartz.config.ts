4ddc055953ac8828448e47347fede223e5f03395
yamshikovvs <iamshchikov.vs@edu.rut-miit.ru>
2024-11-16 12:24:38 +0500

    Quartz sync: Nov 16, 2024, 12:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Цифровой садик",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ru-RU",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
